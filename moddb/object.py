deff834493e564fd1eb4219150b8c6c22ba4b38d
Clement <clement.julia13@gmail.com>
2018-08-25 12:48:46 +0200

    adding skeleton

---
+++ moddb/object.py
@@ -0,0 +1,61 @@
+import enum
+
+class Game:
+    pass
+
+class Mod:
+    pass
+
+class File:
+    pass
+
+class Media:
+    pass
+
+class Article:
+    pass
+
+class Engine:
+    pass
+
+class Team:
+    pass
+
+class Group:
+    pass
+
+class Job:
+    pass
+
+class AddonType(enum.Enum):
+    pass
+
+class Addon:
+    pass
+
+class ThumbnailType(enum.Enum):
+    pass
+
+class Thumbnail:
+    pass
+
+class Comment:
+    pass
+
+class User:
+    pass
+
+class Count:
+    pass
+
+class Profile:
+    pass
+
+class Style:
+    pass
+
+class Statistics:
+    pass
+
+class Blog:
+    pass
