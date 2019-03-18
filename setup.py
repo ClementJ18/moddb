63d41c4a4ca47e80b0e81b953f5d7b0f09c16676
Clement <clement.julia13@gmail.com>
2019-03-18 17:23:09 +0000

    added required stuff

--- setup.py
+++ setup.py
@@ -8,4 +8,10 @@ setup(name = "moddb",
     author = "Clement Julia",
     author_email = "clement.julia13@gmail.com",
     url = "https://github.com/ClementJ18/moddb",
-    packages = ["moddb"])
+    packages = ["moddb"],
+    install_requires=[
+        'beautifulsoup4 == 4.6.3',
+        'requests>=2.20.0',
+        'robobrowser == 0.5.3'
+      ]
+    )
